e4996b021b35f546444ec09fd5255273f56852be
Marnix <marnix@valkdigital.nl>
2024-01-21 12:47:32 +0100

    Make devtools dev only

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
-const { app, BrowserWindow, ipcMain} = require('electron')
-const path = require('node:path')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("node:path");
 
 var win = null;
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-        enableRemoteModule: true,
-        preload: path.join(__dirname, 'preload.js')
-      }
-    })
-    win.webContents.openDevTools()
-    win.loadFile('index.html')
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  const isDev = process.env.NODE_ENV === "development";
+  if (isDev) {
+    win.webContents.openDevTools();
   }
 
-  app.whenReady().then(() => {
-    createWindow()
+  win.loadFile("index.html");
+};
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-      })
-  })
+app.whenReady().then(() => {
+  createWindow();
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
-
-  ipcMain.on("select-directory", (event) => {
-    const { dialog } = require("electron");
-  
-    dialog
-      .showOpenDialog(win, {
-        properties: ["openDirectory"],
-      })
-      .then((result) => {
-        if (!result.canceled && result.filePaths.length > 0) {
-          event.reply("selected-directory", result.filePaths[0]);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.on("select-directory", (event) => {
+  const { dialog } = require("electron");
+
+  dialog
+    .showOpenDialog(win, {
+      properties: ["openDirectory"],
+    })
+    .then((result) => {
+      if (!result.canceled && result.filePaths.length > 0) {
+        event.reply("selected-directory", result.filePaths[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
